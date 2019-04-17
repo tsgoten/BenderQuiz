45686449347071a68ecb54cb19c2418cd2be6e95
Tarang <tsgoten@gmail.com>
2019-04-17 11:40:47 -0400

    updated with app icon

--- BenderQuiz/QuestionViewController.swift
+++ BenderQuiz/QuestionViewController.swift
@@ -223,15 +223,5 @@ class QuestionViewController: UIViewController {
             resultScreenViewController.responses = elementScore
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
